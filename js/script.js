bf75d94720099069b459e805751a7ec166f402a2
alex__kis <fenaethylaamiinloever@gmail.com>
2023-06-16 02:09:19 +0300

    +new sections

--- js/script.js
+++ js/script.js
@@ -1,2 +1,44 @@
 'use strict';
 
+/*==================================== TABS ====================================*/
+
+; (function () {
+	let links = document.querySelectorAll('.tabs__menu-link');
+	let tabs = document.querySelectorAll('.tab');
+
+	for (let i = 0; i < tabs.length; i++) {
+		links[i].addEventListener('click', function () {
+			document.querySelector('.tab--active').classList.remove('tab--active');
+			tabs[i].classList.add('tab--active');
+			document.querySelector('.tabs__menu-link--active').classList.remove('tabs__menu-link--active');
+			links[i].classList.add('tabs__menu-link--active');
+		});
+	}
+	
+})();
+
+/*==================================== ACC ====================================*/
+
+; (function () {
+	
+	let tabs = document.querySelectorAll('.acc__tab');
+	let links = document.querySelectorAll('.acc__link');
+
+
+
+	for (let i = 0; i < tabs.length; i++) {
+		links[i].addEventListener('click', function () {
+			let open = document.querySelector('.acc__tab--active');
+			if (tabs[i].classList.contains('acc__tab--active')) {
+				tabs[i].classList.remove('acc__tab--active');
+
+			} else {
+				tabs[i].classList.add('acc__tab--active');
+				if (open) {
+					open.classList.remove('acc__tab--active');
+
+				}
+			}
+		});
+	}
+})();
